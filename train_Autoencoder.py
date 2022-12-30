370edfcfbef541c08381d2751ba85e065ba6ca80
AsadIsmail <asadismaeel@gmail.com>
2022-12-30 17:13:10 +0000

    imporved training

--- train_Autoencoder.py
+++ train_Autoencoder.py
@@ -38,13 +38,13 @@ def train_Anamoly(latent_dim,train_loader,val_loader,vis_dataset):
     trainer.logger._default_hp_metric = None # Optional logging argument that we don't need
 
     # Check whether pretrained model exists. If yes, load it and skip training
-    pretrained_filename = os.path.join(CHECKPOINT_PATH, f"anamoly_road_{latent_dim}.ckpt")
+    pretrained_filename = os.path.join(ckpt_path, f"anamoly_road_{latent_dim}.ckpt")
     if os.path.isfile(pretrained_filename):
         print("Found pretrained model, loading...")
         model = Autoencoder.load_from_checkpoint(pretrained_filename)
     else:
         model = Autoencoder(latent_dim=latent_dim)
-        trainer.fit(model, train_loader, val_loader)
+    trainer.fit(model, train_loader, val_loader)
     # Test best model on validation and test set
     val_result = trainer.test(model, val_loader, verbose=False)
     #test_result = trainer.test(model, test_loader, verbose=False)
