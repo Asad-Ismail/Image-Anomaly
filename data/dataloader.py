1552a45e34dd4f16cfecaa036563849f43346ddd
AsadIsmail <asadismaeel@gmail.com>
2023-01-05 16:05:33 +0000

    added string docs

--- data/dataloader.py
+++ data/dataloader.py
@@ -2,6 +2,23 @@ from torchvision import datasets, transforms
 import torch
 
 def get_data(batch_sz=32):
+    """
+    Load and preprocess image data for training and validation.
+
+    This function loads image data from the 'train/images' and 'val/images' directories, applies some
+    preprocessing transformations, and returns the resulting data loaders and datasets. The preprocessing
+    transformations include resizing the images to 128x128 pixels, converting them to tensors, and normalizing
+    the pixel values.
+
+    Args:
+    batch_sz (int, optional): batch size for the data loaders. Default is 32.
+
+    Returns:
+    tuple: A tuple containing the following elements:
+    - train_loader (torch.utils.data.DataLoader): DataLoader for the training data.
+    - val_loader (torch.utils.data.DataLoader): DataLoader for the validation data.
+    - train_dataset (torch.utils.data.Dataset): Dataset for the training data.
+    """
     train_dir = '../AnamolyData/train/images'
     val_dir= '../AnamolyData/val/images'
     transform = transforms.Compose([transforms.Resize((128,128)),
